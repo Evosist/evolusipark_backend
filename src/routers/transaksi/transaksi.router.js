f6065e2943193cfce01a3f5fd3b6fc43b7464433
fahmiitmamul <itmamul2004@gmail.com>
2025-06-03 16:38:16 +0700

    fix: update pembatalan transaksi

--- src/routers/transaksi/transaksi.router.js
+++ src/routers/transaksi/transaksi.router.js
@@ -1,10 +1,8 @@
 const router = require('express').Router()
 const transaksiManualMixRouter = require('./transaksi-manual-mix.router')
-const pembatalanTransaksiRouter = require('./pembatalan-transaksi.router')
 const permasalahanAtauPerbaikanRouter = require('./permasalahan-atau-perbaikan.router')
 
 router.use('/manual-mix', transaksiManualMixRouter)
-router.use('/pembatalan-transaksi', pembatalanTransaksiRouter)
 router.use('/permasalahan-atau-perbaikan', permasalahanAtauPerbaikanRouter)
 
 module.exports = router
