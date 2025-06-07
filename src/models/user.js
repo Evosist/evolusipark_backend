bc0cd26195240e32ac55092ca9b502dc67752e55
fahmiitmamul <itmamul2004@gmail.com>
2025-06-07 11:11:56 +0700

    fix: update models

--- src/models/user.js
+++ src/models/user.js
@@ -14,9 +14,9 @@ module.exports = (sequelize, DataTypes) => {
                 as: 'added_by_user',
             })
 
-            user.belongsTo(models.level_akses, {
+            user.belongsTo(models.level_pengguna, {
                 foreignKey: 'level_pengguna_id',
-                as: 'level_akses',
+                as: 'level_pengguna',
             })
 
             user.belongsTo(models.perusahaan, {
