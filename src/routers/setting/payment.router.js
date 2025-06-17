0283eec5a5a517fd1948eb7bcc0b7a9e2240ef66
fahmiitmamul <itmamul2004@gmail.com>
2025-06-17 20:43:37 +0700

    fix: update payment router

--- src/routers/setting/payment.router.js
+++ src/routers/setting/payment.router.js
@@ -2,6 +2,8 @@ const paymentRouter = require('express').Router()
 const paymentController = require('../../controllers/setting/payment.controller')
 
 paymentRouter.get('/', paymentController.getAll)
+paymentRouter.get('/pdf', paymentController.generatePdf)
+paymentRouter.get('/excel', paymentController.generateExcel)
 paymentRouter.get('/:id', paymentController.findOneById)
 paymentRouter.post('/', paymentController.create)
 paymentRouter.patch('/:id', paymentController.update)
