86bfb99cc3e3e5857c422c62fcc048d20bede741
fahmiitmamul <itmamul2004@gmail.com>
2025-06-17 20:28:56 +0700

    update settings

--- src/routers/setting/global.router.js
+++ src/routers/setting/global.router.js
@@ -2,6 +2,8 @@ const globalRouter = require('express').Router()
 const globalController = require('../../controllers/setting/global.controller')
 
 globalRouter.get('/', globalController.getAll)
+globalRouter.get('/pdf', globalController.generatePdf)
+globalRouter.get('/excel', globalController.generateExcel)
 globalRouter.get('/:id', globalController.findOneById)
 globalRouter.post('/', globalController.create)
 globalRouter.patch('/:id', globalController.update)
