7a086a67ee482242c0cb7f27b903ca53873d0665
fahmiitmamul <itmamul2004@gmail.com>
2025-06-25 16:47:58 +0700

    fix: update parameter router

--- src/routers/setting/parameter/parameter.router.js
+++ src/routers/setting/parameter/parameter.router.js
@@ -2,6 +2,8 @@ const parameterRouter = require('express').Router()
 const parameterController = require('../../../controllers/setting/parameter/parameter.controller')
 
 parameterRouter.get('/', parameterController.getAll)
+parameterRouter.get('/pdf', parameterController.generatePdf)
+parameterRouter.get('/excel', parameterController.generateExcel)
 parameterRouter.get('/:id', parameterController.findOneById)
 parameterRouter.post('/', parameterController.create)
 parameterRouter.patch('/:id', parameterController.update)
