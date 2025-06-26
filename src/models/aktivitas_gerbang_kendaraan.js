8c15f367bacad00b1a1a8f878b307376fb59c68c
fahmiitmamul <itmamul2004@gmail.com>
2025-06-26 08:35:56 +0700

    fix: update model

--- src/models/aktivitas_gerbang_kendaraan.js
+++ src/models/aktivitas_gerbang_kendaraan.js
@@ -9,18 +9,22 @@ module.exports = (sequelize, DataTypes) => {
          */
         static associate(models) {
             // define association here
+
+            aktivitas_gerbang_kendaraan.belongsTo(models.kendaraan, {
+                foreignKey: 'kendaraan_id',
+            })
         }
     }
     aktivitas_gerbang_kendaraan.init(
         {
-            tiket_id: DataTypes.INTEGER,
+            tiket: DataTypes.STRING,
             plat_nomor: DataTypes.STRING,
             kendaraan_id: DataTypes.INTEGER,
             waktu: DataTypes.DATE,
             lokasi_gerbang: DataTypes.STRING,
-            buka_atau_tutup: DataTypes.ENUM('Buka', 'Tutup'),
-            petugas_id: DataTypes.INTEGER,
-            status_palang: DataTypes.ENUM('Sukses', 'Gagal'),
+            buka_atau_tutup: DataTypes.STRING,
+            petugas: DataTypes.STRING,
+            status_palang: DataTypes.STRING,
         },
         {
             sequelize,
