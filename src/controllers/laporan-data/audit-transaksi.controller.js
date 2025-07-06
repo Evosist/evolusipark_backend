41ad532139416546dbe8856c0d65ac75f35c0484
fahmiitmamul <itmamul2004@gmail.com>
2025-07-06 19:28:09 +0700

    fix: update controller

--- src/controllers/laporan-data/audit-transaksi.controller.js
+++ src/controllers/laporan-data/audit-transaksi.controller.js
@@ -5,12 +5,24 @@ module.exports = {
     getAllAuditTransaksiKendaraanKeluar: async (req, res) => {
         try {
             const [results] = await sequelize.query(`
-                SELECT t.tanggal_masuk as tanggal,
-                       t.no_tiket_atau_tiket_manual as no_tiket_atau_id_transaksi,
-                       t.nomor_polisi as nopol,
-                       m.nama as nama_member,
-                       
-              `)
+            SELECT
+            tm.tanggal_keluar::date AS "Tanggal",
+            'Casual' AS "Kategori",
+            tm.no_tiket_atau_tiket_manual AS "NoTiket",
+            tm.nomor_polisi AS "Nopol",
+            '-' AS "NamaMember",
+            tm.parkir AS "TarifAsli",
+            pv.nama AS "NamaVoucher",
+            COALESCE(dv.tarif, 0) AS "PotonganVoucher",
+            (CAST(tm.parkir AS INTEGER) - COALESCE(dv.tarif, 0)) AS "TarifDibayar",
+            pay.jenis_payment AS "Pembayaran"
+            FROM transaksi_manuals tm
+            LEFT JOIN data_vouchers dv ON tm.id_data_voucher = dv.id
+            LEFT JOIN produk_vouchers pv ON dv.produk_voucher_id = pv.id
+            LEFT JOIN payments pay ON tm.jenis_pembayaran_id = pay.id
+            WHERE tm.tanggal_keluar BETWEEN '2025-06-29' AND '2025-07-06'
+            ORDER BY tm.tanggal_keluar DESC  
+            `)
 
             return res.json({
                 success: true,
@@ -25,11 +37,17 @@ module.exports = {
     getAllAuditTransaksiManual: async (req, res) => {
         try {
             const [results] = await sequelize.query(`
-                SELECT t.tanggal_masuk as tanggal,
-                       t.no_tiket_atau_tiket_manual as no_tiket_atau_id_transaksi,
-                       t.nomor_polisi as nopol,
-                       m.nama as nama_member,
-                       
+              SELECT
+              pos.kode AS "Pos",
+              u.nama AS "NamaPetugas",
+              COUNT(tm.id) AS "QtyTransaksi",
+              SUM(CAST(regexp_replace(tm.parkir, '[^0-9]', '', 'g') AS INTEGER)) AS "TotalNominal"
+              FROM transaksi_manuals tm
+              LEFT JOIN users u ON tm.petugas_id = u.id
+              LEFT JOIN pos ON tm.pintu_keluar_id = pos.id
+              WHERE tm.tanggal_keluar BETWEEN '2025-06-29' AND '2025-07-06'
+              GROUP BY pos.kode, u.nama
+              ORDER BY "QtyTransaksi" DESC          
               `)
 
             return res.json({
@@ -44,16 +62,24 @@ module.exports = {
     getAllAuditTransaksiPenggunaanVoucher: async (req, res) => {
         try {
             const [results] = await sequelize.query(`
-                SELECT t.tanggal_masuk as tanggal,
-                       t.no_tiket_atau_tiket_manual as no_tiket_atau_id_transaksi,
-                       t.nomor_polisi as nopol,
-                       m.nama as nama_member,
-                       
+                 SELECT
+                 pv.nama AS "NamaVoucher",
+                 CONCAT('Rp ', COALESCE(dv.tarif, 0)) AS "PotonganVoucher",
+                 u.nama AS "NamaPetugasPos",
+                 COUNT(tm.id) AS "QtyVoucherDigunakan"
+                 FROM transaksi_manuals tm
+                 INNER JOIN data_vouchers dv ON tm.id_data_voucher = dv.id
+                 INNER JOIN produk_vouchers pv ON dv.produk_voucher_id = pv.id
+                 INNER JOIN users u ON tm.petugas_id = u.id
+                 WHERE tm.tanggal_keluar::date BETWEEN '2025-06-29' AND '2025-07-06'
+                 GROUP BY pv.nama, dv.tarif, u.nama
+                 ORDER BY COUNT(tm.id) DESC
               `)
 
             return res.json({
                 success: true,
-                message: 'Get all audit transaksi manual successfully',
+                message:
+                    'Get all audit transaksi penggunaan voucher successfully',
                 results: results,
             })
         } catch (err) {
@@ -63,16 +89,22 @@ module.exports = {
     getAllAuditPembatalanTransaksi: async (req, res) => {
         try {
             const [results] = await sequelize.query(`
-                SELECT t.tanggal_masuk as tanggal,
-                       t.no_tiket_atau_tiket_manual as no_tiket_atau_id_transaksi,
-                       t.nomor_polisi as nopol,
-                       m.nama as nama_member,
-                       
+                SELECT
+                pos.kode AS "Pos",
+                u.nama AS "NamaPetugas",
+                COUNT(ltb.id) AS "QtyTransaksiDibatalkan",
+                SUM(CAST(regexp_replace(ltb.total_pembayaran, '[^0-9]', '', 'g') AS INTEGER)) AS "TotalNominalPembatalan"
+                FROM laporan_transaksi_batals ltb
+                LEFT JOIN users u ON ltb.petugas_id = u.id
+                LEFT JOIN pos ON ltb.gerbang_keluar_id = pos.id
+                WHERE ltb."createdAt"::date BETWEEN '2025-06-29' AND '2025-07-06'
+                GROUP BY pos.kode, u.nama
+                ORDER BY COUNT(ltb.id) DESC
               `)
 
             return res.json({
                 success: true,
-                message: 'Get all audit transaksi manual successfully',
+                message: 'Get all audit pembatalan transaksi successfully',
                 results: results,
             })
         } catch (err) {
--- src/controllers/laporan-data/settlement-cashless.controller.js
+++ src/controllers/laporan-data/settlement-cashless.controller.js
@@ -5,16 +5,50 @@ module.exports = {
     getAll: async (req, res) => {
         try {
             const [results] = await sequelize.query(`
-                SELECT t.tanggal_masuk as tanggal,
-                       t.no_tiket_atau_tiket_manual as no_tiket_atau_id_transaksi,
-                       t.nomor_polisi as nopol,
-                       m.nama as nama_member,
-                       
+                  SELECT
+                  tm.no_tiket_atau_tiket_manual AS "NoTiket",
+                  tm.tanggal_keluar AS "TanggalKeluar",
+                  pos_out.kode AS "PintuKeluar",
+                  tm.nomor_polisi AS "Nopol",
+                  k.nama_kendaraan AS "Kendaraan",
+                  tm."interval" AS "Interval",
+                  tm.parkir AS "Tarif",
+                  (tm.jumlah_denda_tiket + tm.jumlah_denda_stnk) AS "Denda",
+                  COALESCE(td.tipe_denda, 'Tidak Ada') AS "TipeDenda",
+                  pay.jenis_payment AS "Pembayaran",
+                  CASE
+                    WHEN pay.jenis_payment ILIKE '%QRIS%' THEN 'GoPay'
+                    WHEN pay.jenis_payment ILIKE '%VA%' THEN 'Mandiri VA'
+                    WHEN pay.jenis_payment ILIKE '%Wallet%' THEN 'OVO'
+                    ELSE 'Credit Card'
+                  END AS "Channel",
+                  '-' AS "VA_QR",
+                  u.nama AS "Petugas",
+                  s.nama_shift AS "Shift",
+                  CONCAT('mtd-', tm.id) AS "TransactionID",
+                  CONCAT('ORD-', tm.id) AS "OrderID",
+                  tm.tanggal_keluar AS "TransactionTime",
+                  TO_DATE('2025-07-10', 'YYYY-MM-DD') AS "SettlementTime",
+                  CASE
+                    WHEN tm.is_active = true THEN 'Settled'
+                    WHEN tm.is_active = false AND tm.denda = true THEN 'Gagal'
+                    ELSE 'Pending'
+                  END AS "SettlementStatus",
+                  CONCAT('st_', TO_CHAR(tm.tanggal_keluar, 'YYYYMMDD'), '.csv') AS "FileSettlement"
+                  FROM transaksi_manuals tm
+                  LEFT JOIN pos pos_out ON tm.pintu_keluar_id = pos_out.id
+                  LEFT JOIN kendaraans k ON tm.kendaraan_id = k.id
+                  LEFT JOIN tipe_dendas td ON tm.tipe_denda_id = td.id
+                  LEFT JOIN payments pay ON tm.jenis_pembayaran_id = pay.id
+                  LEFT JOIN users u ON tm.petugas_id = u.id
+                  LEFT JOIN shifts s ON tm.shift_id = s.id
+                  WHERE tm.tanggal_keluar BETWEEN '2025-06-29' AND '2025-07-06'
+                  ORDER BY tm.tanggal_keluar DESC
               `)
 
             return res.json({
                 success: true,
-                message: 'Get all audit transaksi manual successfully',
+                message: 'Get all settlement cashless successfully',
                 results: results,
             })
         } catch (err) {
