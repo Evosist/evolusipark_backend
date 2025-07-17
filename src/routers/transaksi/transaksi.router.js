f149e29bcd7b07332f9f4e4dcf3c27e409962191
fahmiitmamul <itmamul2004@gmail.com>
2025-07-17 20:12:37 +0700

    fix: update controller

--- src/routers/transaksi/transaksi.router.js
+++ src/routers/transaksi/transaksi.router.js
@@ -4,7 +4,6 @@ const transaksiController = require('../../controllers/transaksi/transaksi.contr
 transaksiRouter.get('/', transaksiController.getAll)
 transaksiRouter.get('/:id', transaksiController.findOneById)
 transaksiRouter.post('/', transaksiController.create)
-transaksiRouter.post('/laporan', transaksiController.createLaporan)
 transaksiRouter.patch('/cancel-transaksi', transaksiController.updateTransaksi)
 transaksiRouter.patch('/:id', transaksiController.update)
 transaksiRouter.delete('/:id', transaksiController.delete)
