3b725fc108a5957112e235de391d3c5834984ccd
fahmiitmamul <itmamul2004@gmail.com>
2025-07-27 15:17:52 +0700

    fix: placement router

--- src/routers/transaksi/index.router.js
+++ src/routers/transaksi/index.router.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const transaksiRouter = require('./transaksi.router')
 const permasalahanAtauPerbaikanRouter = require('./permasalahan-atau-perbaikan.router')
 
-router.use('/', transaksiRouter)
 router.use('/permasalahan-atau-perbaikan', permasalahanAtauPerbaikanRouter)
+router.use('/', transaksiRouter)
 
 module.exports = router
