f3c14519626c0b3bbc6dbc3aecf1db04db58e3f0
Zen Zalepik <51511976+zenzalepik@users.noreply.github.com>
2025-07-28 03:39:37 +0700

    Update index.router.js

--- src/routers/transaksi/index.router.js
+++ src/routers/transaksi/index.router.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const transaksiRouter = require('./transaksi.router')
 const permasalahanAtauPerbaikanRouter = require('./permasalahan-atau-perbaikan.router')
 
-router.use('/permasalahan-atau-perbaikan', permasalahanAtauPerbaikanRouter)
 router.use('/', transaksiRouter)
+router.use('/permasalahan-atau-perbaikan', permasalahanAtauPerbaikanRouter)
 
 module.exports = router
