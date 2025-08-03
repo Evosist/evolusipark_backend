cb760e0cb3a7f7a71b4c5a32929c3eedfa49b68e
fahmiitmamul <itmamul2004@gmail.com>
2025-08-03 11:35:04 +0700

    fix: update database

--- src/configs/db.js
+++ src/configs/db.js
@@ -11,7 +11,13 @@ module.exports = {
         port: process.env.DB_PORT,
         dialect: 'postgres',
         dialectModule: pg,
-        logging: false
+        logging: false,
         // ⚠️ Hilangkan dialectOptions jika PostgreSQL tidak pakai SSL
-    }
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+    },
 }
