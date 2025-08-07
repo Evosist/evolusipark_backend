2f6c19915924d59f328a8d5d26aa83f33d2a815e
Zen Zalepik <51511976+zenzalepik@users.noreply.github.com>
2025-08-07 18:40:43 +0700

    feat: riwayat transaksi manual

--- src/routers/transaksi/transaksi.router.js
+++ src/routers/transaksi/transaksi.router.js
@@ -1,11 +1,19 @@
 const transaksiRouter = require('express').Router()
 const transaksiController = require('../../controllers/transaksi/transaksi.controller')
+const transaksiTipeManualController = require('../../controllers/transaksi/transaksiTipeManual.controller')
 
+// Routes utama
 transaksiRouter.get('/', transaksiController.getAll)
 transaksiRouter.get('/:id', transaksiController.findOneById)
 transaksiRouter.post('/', transaksiController.create)
 transaksiRouter.patch('/cancel-transaksi', transaksiController.cancelTransaksi)
 transaksiRouter.patch('/:id', transaksiController.update)
 transaksiRouter.delete('/:id', transaksiController.delete)
+
+// ✅ Route baru untuk riwayat manual
+transaksiRouter.get(
+  '/manual/history',
+  transaksiTipeManualController.getAllRiwayatTransaksiManual
+)
 
 module.exports = transaksiRouter
