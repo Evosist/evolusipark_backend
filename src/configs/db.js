ae0b7ccae077a305930d66e730a8f146c8b7e976
Zen Zalepik <51511976+zenzalepik@users.noreply.github.com>
2025-08-12 17:18:25 +0700

    ignore

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,7 +1,6 @@
 const pg = require('pg')
 
-const envPath = `.env.${process.env.NODE_ENV || 'development'}`
-require('dotenv').config({ path: envPath })
+require('dotenv').config({ path: '.env' })
 
 module.exports = {
     development: {
@@ -12,42 +11,7 @@ module.exports = {
         port: process.env.DB_PORT,
         dialect: 'postgres',
         dialectModule: pg,
-<<<<<<< HEAD
-        logging: false
+        logging: false,
         // ⚠️ Hilangkan dialectOptions jika PostgreSQL tidak pakai SSL
-    }
-=======
-        logging: false,
     },
-    staging: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'postgres',
-        dialectModule: pg,
-        logging: false,
-        dialectOptions: {
-            ssl: {
-                rejectUnauthorized: false,
-            },
-        },
-    },
-    production: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'postgres',
-        dialectModule: pg,
-        logging: false,
-        dialectOptions: {
-            ssl: {
-                rejectUnauthorized: false,
-            },
-        },
-    },
->>>>>>> ab960c44f5a6333bc052f7c8c30b99f7a22ae700
 }
