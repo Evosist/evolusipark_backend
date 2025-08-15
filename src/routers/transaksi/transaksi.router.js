f5bae8ba9fae0a5205e4c1f753cbfe5bc3c5f2d0
fahmiitmamul <itmamul2004@gmail.com>
2025-08-15 07:24:42 +0700

    fix: update transaksi router

--- src/routers/transaksi/transaksi.router.js
+++ src/routers/transaksi/transaksi.router.js
@@ -4,22 +4,36 @@ const transaksiTipeManualController = require('../../controllers/transaksi/trans
 const transaksiTipeTunaiController = require('../../controllers/transaksi/transaksiTipeTunai.controller')
 const transaksiTipeBankController = require('../../controllers/transaksi/transaksiTipeBank.controller')
 const transaksiTipeQrisController = require('../../controllers/transaksi/transaksiTipeQris.controller')
-const transaksiTipeMemberController = require('../../controllers/transaksi/transaksiTipeMember.controller');
+const transaksiTipeMemberController = require('../../controllers/transaksi/transaksiTipeMember.controller')
 
 // ✅ Route baru untuk riwayat manual
-transaksiRouter.get('/riwayat-manual', transaksiTipeManualController.getAllRiwayatTransaksiManual)
-transaksiRouter.get('/riwayat-tunai', transaksiTipeTunaiController.getAllRiwayatTransaksiTunai)
-transaksiRouter.get('/riwayat-bank', transaksiTipeBankController.getAllRiwayatTransaksiBank)
-transaksiRouter.get('/riwayat-qris', transaksiTipeQrisController.getAllRiwayatTransaksiQris)
-transaksiRouter.get('/riwayat-member', transaksiTipeMemberController.getAllRiwayatTransaksiMember);
+transaksiRouter.get(
+    '/riwayat-manual',
+    transaksiTipeManualController.getAllRiwayatTransaksiManual
+)
+transaksiRouter.get(
+    '/riwayat-tunai',
+    transaksiTipeTunaiController.getAllRiwayatTransaksiTunai
+)
+transaksiRouter.get(
+    '/riwayat-bank',
+    transaksiTipeBankController.getAllRiwayatTransaksiBank
+)
+transaksiRouter.get(
+    '/riwayat-qris',
+    transaksiTipeQrisController.getAllRiwayatTransaksiQris
+)
+transaksiRouter.get(
+    '/riwayat-member',
+    transaksiTipeMemberController.getAllRiwayatTransaksiMember
+)
 
 // Routes utama
 transaksiRouter.get('/', transaksiController.getAll)
 transaksiRouter.get('/:id', transaksiController.findOneById)
 transaksiRouter.post('/', transaksiController.create)
 transaksiRouter.patch('/cancel-transaksi', transaksiController.cancelTransaksi)
-transaksiRouter.patch('/:id', transaksiController.update)
+transaksiRouter.patch('/', transaksiController.update)
 transaksiRouter.delete('/:id', transaksiController.delete)
-
 
 module.exports = transaksiRouter
